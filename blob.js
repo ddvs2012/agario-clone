e8acc9e908f8b7e64134b0b5e80776cce80eab0f
Daniel Alexander <ddvs2012@gmail.com>
2018-05-24 20:52:56 -0400

    first commit

---
+++ blob.js
@@ -0,0 +1,16 @@
+function Blob(x, y, r) {
+	this.pos = createVector(x, y);
+	this.r = r;
+
+	this.update = function() {
+		var vel = createVector(mouseX, mouseY);
+		vel.sub(this.pos);
+		vel.setMag(3);
+		this.pos.add(vel);
+	}
+
+	this.show = function(){
+		fill(255);
+		ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2);
+	}
+}
