d948c355f316c885dbc9ee29456d09a76a425a7b
Daniel Alexander <ddvs2012@gmail.com>
2018-05-26 23:29:12 -0400

    Day 2, added size relative zoom out perspective, added lerp to smooth movements of perspective and growth

--- blob.js
+++ blob.js
@@ -1,12 +1,26 @@
+//constructor(s) for blobs
+
 function Blob(x, y, r) {
 	this.pos = createVector(x, y);
 	this.r = r;
+	this.vel = createVector(0,0);
 
-	this.update = function() {
-		var vel = createVector(mouseX, mouseY);
-		vel.sub(this.pos);
-		vel.setMag(3);
-		this.pos.add(vel);
+	this.update = function() { // updates the screen
+		var newvel = createVector(mouseX-width/2, mouseY-height/2);  //point of interest
+		newvel.setMag(3);	//acceleration?
+		this.vel.lerp(newvel, 0.1); // smooths turning
+		this.pos.add(this.vel);	//??
+	}
+
+	this.eats = function(other){	//handles growth/blob removal
+		var d = p5.Vector.dist(this.pos, other.pos);	//?
+		if (d < this.r + other.r*.5){
+			var area = PI * this.r * this.r + PI * other.r * other.r;	//calculating new area
+			this.r = sqrt(area / PI); //assigning new area to player
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	this.show = function(){
--- sketch.js
+++ sketch.js
@@ -1,20 +1,39 @@
-var blob;
-var blobs = [];
+//draws objects
+
+var blob;	//player
+var blobs = []; //container for blobs to eat
+var zoom = 1;
 
 function setup() {
-  createCanvas(600, 600);
-  blob = new Blob(width/2, height/2, 64);
-  for (var i = 0; i < 10; i++){
-  	blobs[i] = new Blob(random(width), random(height), 16);
+  createCanvas(800, 800);	//create background
+  blob = new Blob(0, 0, 64);
+  for (var i = 0; i < 1000; i++){	//create blobs to eat
+  	var x =random(-width, width)
+  	var y =random(-height, height)
+  	blobs[i] = new Blob(x, y, 16);
   } 
 
 }
 
 function draw() {
   background(0);
+
+  translate(width/2, height/2);	// handles zoom distance
+  var newzoom = 64 / blob.r;  
+  zoom = lerp(zoom, newzoom, 0.1);
+  scale(zoom);
+  translate(-blob.pos.x, -blob.pos.y);
+
+  for (var i = blobs.length - 1; i >= 0; i--) {
+  	blobs[i].show(0, 1);
+  	if (blob.eats(blobs[i])) {
+  		blobs.splice(i, 1);
+  	} 
+  }
   blob.show();
   blob.update();
-  for (var i = 0; i < blobs.length; i++){
+  for (var i = 0; i < blobs.length; i++){	//shows blobs created in setup
   	blobs[i].show();
   }
+
 }
